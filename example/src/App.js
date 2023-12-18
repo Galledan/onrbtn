237293c52916180c983099471bb572dd87cad6cd
Galledan <onrclklr@outlook.com>
2023-12-18 17:33:12 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'onrbtn'
+import 'onrbtn/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
